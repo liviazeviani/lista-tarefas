0b30656adc9d8e07308406f8d019b1968f818cc5
liviapz <liviapavinizeviani@gmail.com>
2024-07-15 20:18:30 -0300

    criando os endpoints no controller

--- src/main/java/br/com/liviazeviani/lista_tarefas/controller/TodoController.java
+++ src/main/java/br/com/liviazeviani/lista_tarefas/controller/TodoController.java
@@ -7,6 +7,12 @@ import br.com.liviazeviani.lista_tarefas.service.TodoService;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,22 +27,26 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    List<Todo> create(Todo todo){
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
         return todoService.create(todo);
         
     }
 
+    @GetMapping
     List<Todo> list(){
         return todoService.list();
 
     }
 
-    List<Todo> update(Todo todo){
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
         return todoService.update(todo);
 
     }
 
-    List<Todo> delete(Long id){
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
         return todoService.delete(id);
 
     }
